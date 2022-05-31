9d9a3313c4ffe90a0c0c60e51b90bdf74da9b090
Lozjan <lj47085@ubt-uni.net>
2022-05-31 11:36:34 +0200

    conditional rendering for Login state

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,13 +11,13 @@ import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 function Navbar() {
+  const [auth, setAuth] = useState();
   const [toggle, setToggle] = useState(false);
 
   const handleToggle = () => setToggle(!toggle);
 
   return (
     <>
-
       {/*----------------------------------FIRST NAVBAR */}
       <Container fluid className="bg-dark py-2">
         <Container className="d-flex align-items-center">
@@ -41,38 +41,71 @@ function Navbar() {
             </NavbarComponent.Collapse>
           </NavbarComponent>
 
-          <div className="text-white d-flex justify-content-end w-50">
-            <button
-              onClick={handleToggle}
-              className="bg-white rounded-circle"
-              style={{ width: "50px", height: "50px", fontSize: "30px" }}
-            >
-              <Icon icon={faUser} />
-            </button>
-          </div>
+          {auth ? ( //if it's logged in
+            <div className="text-white d-flex justify-content-end w-50">
+              <button
+                onClick={handleToggle}
+                className="bg-white rounded-circle"
+                style={{ width: "50px", height: "50px", fontSize: "30px" }}
+              >
+                <Icon icon={faUser} />
+              </button>
+            </div>
+          ) : (
+            <div className="d-flex justify-content-end w-50">
+              <Link to="/login" className="nav-link text-white">LOGIN</Link>
+            </div>
+          )}
         </Container>
       </Container>
 
       {/*----------------------------------PROFILE MENU */}
-      <Offcanvas show={toggle} onHide={handleToggle} placement="end" style={{width: '250px', height: '210px'}}>
-        <Offcanvas.Header closeButton style={{paddingBottom: '0'}}>
+      <Offcanvas
+        show={toggle}
+        onHide={handleToggle}
+        placement="end"
+        style={{ width: "250px", height: "210px" }}
+      >
+        <Offcanvas.Header closeButton style={{ paddingBottom: "0" }}>
           <Offcanvas.Title className="d-flex">
-
-            <Container className="border rounded-circle d-flex justify-content-center align-items-center bg-dark text-white" style={{width: '60px', height: '50px'}}>
+            <Container
+              className="border rounded-circle d-flex justify-content-center align-items-center bg-dark text-white"
+              style={{ width: "60px", height: "50px" }}
+            >
               <Icon icon={faUser} />
             </Container>
             <Container>
-              <p style={{fontSize: '17px'}}>Filan Fisteku</p>
-              <p style={{fontSize: '14px', marginTop: '-18px'}}>username1</p>
+              <p style={{ fontSize: "17px" }}>Filan Fisteku</p>
+              <p style={{ fontSize: "14px", marginTop: "-18px" }}>username1</p>
             </Container>
-          
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <div className="border" style={{marginTop: '-15px'}}></div>
-          <Link to="/" onClick={handleToggle} className="text-dark" style={{textDecoration: 'none'}}><h6 className="mt-3">Profile</h6></Link>
-          <Link to="/admin" onClick={handleToggle} className="text-dark" style={{textDecoration: 'none'}}><h6 className="mt-3">Admin</h6></Link>
-          <Link to="/" onClick={handleToggle} className="text-dark" style={{textDecoration: 'none'}}><h6 className="mt-3">Log Out</h6></Link>
+          <div className="border" style={{ marginTop: "-15px" }}></div>
+          <Link
+            to="/"
+            onClick={handleToggle}
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            <h6 className="mt-3">Profile</h6>
+          </Link>
+          <Link
+            to="/admin"
+            onClick={handleToggle}
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            <h6 className="mt-3">Admin</h6>
+          </Link>
+          <Link
+            to="/"
+            onClick={handleToggle}
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            <h6 className="mt-3">Log Out</h6>
+          </Link>
         </Offcanvas.Body>
       </Offcanvas>
 

adaaefdd3d48eac7c60a73d17aa2360792f245f8
Lozjan <lj47085@ubt-uni.net>
2022-06-02 23:12:02 +0200

    delete user function

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container, Table } from "react-bootstrap";
 import AdminHeader from "../../components/Admin/Header";
+import { useParams } from "react-router-dom";
 
 function Users() {
   const [users, setUsers] = useState();
 
-  var apiURL = "http://localhost:4000/users";
-
   useEffect(() => {
     const getUsers = async () => {
       try {
+        var apiURL = "http://localhost:4000/users";
         const response = await axios.get(apiURL);
         setUsers(response.data);
       } catch (error) {
@@ -19,6 +19,18 @@ function Users() {
     };
     getUsers();
   }, []);
+
+  async function deleteUser(id){
+    var apiURL = `http://localhost:4000/users/delete/${id}`;
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      try {
+        await axios.delete(apiURL);
+        setUsers(users.filter(el => el._id !== id));
+      } catch(error) {
+        console.log(error)
+      }
+    }
+  }
 
   return (
     <>
@@ -48,7 +60,7 @@ function Users() {
                     <td>{user.email}</td>
                     <td className="d-flex justify-content-around">
                         <button className="btn btn-primary my-1">Edit</button>
-                        <button className="btn btn-danger my-1">Delete</button>
+                        <button className="btn btn-danger my-1" onClick={() => deleteUser(user._id)}>Delete</button>
                     </td>
                   </tr>
                 );

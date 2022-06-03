9c7b5ecef5b2b3f843c7f0febc75a63dc35f09bc
Lozjan <lj47085@ubt-uni.net>
2022-06-03 15:06:44 +0200

    Book delete function

--- server/routes/book-route.js
+++ server/routes/book-route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 const Router = express.Router()
 const BookModel = require('../models/Book')
+const fs = require('fs')
 
 //CREATE
 Router.route('/create').post(fileUpload(), (req, res, next) => {
@@ -32,7 +33,6 @@ Router.route('/').get((req, res, next) => {
     BookModel.find((error, result) => {
         if (error) return next(error);
         res.json(result);
-        console.log(result);
     })
 })
 
@@ -56,10 +56,19 @@ Router.route('/update/:id').post((req, res, next) => {
 
 //DELETE
 Router.route('/delete/:id').delete((req, res, next) => {
-   BookModel.findByIdAndDelete(req.params.id, (error, result) => {
+
+   BookModel.findByIdAndDelete(req.params.id, (error, data) => {
        if (error) return next(error)
+
+       try {
+        let img = data.Image; //gets the Image which is the imgname stored in database
+        const targetFolder = `.././client/public/uploads/${img}`;
+        fs.unlinkSync(targetFolder);
+       } catch(e){
+           console.log(e);
+       }
        res.status(200).json({
-           msg: result
+           msg: data
        })
    })
 })
